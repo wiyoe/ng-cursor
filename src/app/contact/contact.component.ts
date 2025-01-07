b229af018a81b676fb9a8470a44d4f040ae1c54f
Recep H <recep.hidir@gmail.com>
2025-01-07 10:18:23 +0300

    Refactor contact component to support dynamic map location: Update HTML to use a safe resource URL for Google Maps based on the current language. Enhance TypeScript logic to determine the map location for Turkish and English languages, improving internationalization and user experience.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { SafePipe } from '../pipes/safe.pipe';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, SafePipe],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {} 
+export class ContactComponent {
+  constructor(private translate: TranslateService) {}
+
+  get mapLocation(): string {
+    return this.translate.currentLang === 'tr' 
+      ? 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d195884.0584252303!2d32.62259520709338!3d39.90329559177694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d347d520732db1%3A0xbdc57b0c0842b8d!2sAnkara%2C%20Turkey!5e0!3m2!1sen!2sus!4v1709669151646!5m2!1sen!2sus'
+      : 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9916256937595!2d2.2922926156740714!3d48.858370079287475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sEiffel%20Tower!5e0!3m2!1sen!2sus!4v1647951865986!5w200!5h200';
+  }
+} 
---
+++ src/app/pipes/safe.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml, SafeStyle, SafeScript, SafeUrl, SafeResourceUrl } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safe',
+  standalone: true
+})
+export class SafePipe implements PipeTransform {
+  constructor(protected sanitizer: DomSanitizer) {}
+
+  transform(value: string, type: string): SafeHtml | SafeStyle | SafeScript | SafeUrl | SafeResourceUrl {
+    switch (type) {
+      case 'resourceUrl':
+        return this.sanitizer.bypassSecurityTrustResourceUrl(value);
+      default:
+        throw new Error(`Invalid safe type specified: ${type}`);
+    }
+  }
+} 
